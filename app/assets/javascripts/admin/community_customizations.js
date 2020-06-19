8ec1094ff72c312e434dfe3df4002481bd1357db
cpcwood <cpcwood@outlook.com>
2020-06-19 18:28:24 +0100

    add javascript to community_customizations.js to add new typed slogan containers. Add typed slogan translations to i18n js config for loading

--- app/assets/javascripts/admin/community_customizations.js
+++ app/assets/javascripts/admin/community_customizations.js
@@ -55,13 +55,42 @@ window.ST = window.ST || {};
     initializeCustomizationFormValidation();
   };
 
-  // var appendNewTypedSloganElement(locale, parentContainer) {
-  //   // append new typed slogan element in parent container
-  // }
+  const generateNewTypedSloganFormElement = (locale, idNum) => {
+    I18n.locale = locale
+    let newTypedSloganTemplate = `
+    <input type="checkbox" name="new_typed_slogans[slogans][][ignore]" id="new-typed-slogan-${locale}-${idNum}" value="1" class="typed-slogan-checkbox">
+    <input type="hidden" name="new_typed_slogans[slogans][][locale]" id="new_typed_slogans_slogans__locale" value="${locale}">
+    <div class="name-locale-text_field typed-slogan-textbox">
+      <input type="text" name="new_typed_slogans[slogans][][typed_slogan_text]" id="new_typed_slogans_slogans__typed_slogan_text" placeholder="${I18n.t("admin.communities.edit_details.community_typed_slogan_add_placeholder")}">
+    </div>
+    <label class="remove-typed-slogan-button" for="new-typed-slogan-${locale}-${idNum}">
+      <div class="remove-typed-slogan-remove">
+        ${I18n.t("admin.communities.edit_details.community_typed_slogan_remove")}
+      </div>
+      <div class="remove-typed-slogan-undo">
+        ${I18n.t("admin.communities.edit_details.community_typed_slogan_undo")}
+      </div>
+    </label>
+    `
+    let newTypedSloganElement = document.createElement("div")
+    newTypedSloganElement.classList.add('typed-slogan-container')
+    newTypedSloganElement.innerHTML = newTypedSloganTemplate
+    return newTypedSloganElement
+  }
 
-  // module.addNewTypedSloganButton(locale) {
-  //   // For all button elements add onclick functionality
-  //   // find parent container
-  //   // append new typed slogan element from template
-  // }
+  module.addNewTypedSlogan = () => {
+    let counter = 1
+    let addTypedSloganButtons = document.getElementsByClassName('add-typed-slogan-button')
+    for (let i = 0; i < addTypedSloganButtons.length; i++) {
+      addTypedSloganButtons[i].addEventListener('click', (e) => {
+        e.preventDefault()
+        let thisButton = e.target
+        let buttonContainer = thisButton.parentElement.parentElement
+        let translationContainer = thisButton.parentElement.parentElement.parentElement
+        let locale = thisButton.getAttribute('data-locale')
+        let newTypedSloganFormElement = generateNewTypedSloganFormElement(locale, counter++)
+        translationContainer.insertBefore(newTypedSloganFormElement, buttonContainer)
+      })
+    }
+  }
 })(window.ST);
