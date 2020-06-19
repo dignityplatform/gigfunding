dcf05e6426aa943cdd74cba2cd3971abd79a1879
cpcwood <cpcwood@outlook.com>
2020-06-19 01:29:03 +0100

    add test for destroying typed_slogan

--- spec/controllers/admin/community_customizations_controller_spec.rb
+++ spec/controllers/admin/community_customizations_controller_spec.rb
@@ -6,6 +6,7 @@ describe Admin::CommunityCustomizationsController, type: :controller do
   
 
   describe '#update_details' do
+
     before(:each) do
       @request.host = "#{community.ident}.lvh.me"
       @request.env[:current_marketplace] = community
@@ -19,14 +20,24 @@ describe Admin::CommunityCustomizationsController, type: :controller do
     end
 
     describe 'typed_slogan' do
+
+      community_customizations_params = {"en"=> {"name"=>"", "slogan"=>"", "description"=>"", "search_placeholder"=>"", "transaction_agreement_label"=>"", "transaction_agreement_content"=>""}}
+
       it 'updates the text of typed slogans' do
-        community_customizations_params = {"en"=> {"name"=>"", "slogan"=>"", "description"=>"", "search_placeholder"=>"", "transaction_agreement_label"=>"", "transaction_agreement_content"=>""}}
         typed_slogan_params = {"#{@typed_slogan1.id}": {"typed_slogan_text": "updatedtext1"}, "#{@typed_slogan2.id}": {"typed_slogan_text": "updatedtext2"}}
         full_post_params = {"typed_slogan": typed_slogan_params, "community_customizations": community_customizations_params}
         post :update_details, params: full_post_params
-        
+
         expect(@typed_slogan1.reload.typed_slogan_text).to eq('updatedtext1')
         expect(@typed_slogan2.reload.typed_slogan_text).to eq('updatedtext2')
+      end
+
+      it 'destroys typed slogan if param received from checked box' do
+        typed_slogan_params = {"#{@typed_slogan1.id}": {"typed_slogan_text": "test1", "remove": "1"}, "#{@typed_slogan2.id}": {"typed_slogan_text": "test2"}}
+        full_post_params = {"typed_slogan": typed_slogan_params, "community_customizations": community_customizations_params}
+        post :update_details, params: full_post_params
+
+        expect {@typed_slogan1.reload}.to raise_error ActiveRecord::RecordNotFound
       end
     end
   end
