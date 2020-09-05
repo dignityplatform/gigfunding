53640f0ebb04fac7f6da2f3c98e073ee380e7ae5
cpcwood <cpcwood@outlook.com>
2020-09-05 13:10:21 +0100

    add controller create method error messages

--- app/controllers/admin/causes_controller.rb
+++ app/controllers/admin/causes_controller.rb
@@ -11,7 +11,7 @@ class Admin::CausesController < Admin::AdminBaseController
     if @cause.save
       redirect_to admin_causes_path, notice:  "Cause created"
     else
-      flash[:error] = "Cause creation failed"
+      flash[:error] = @cause.errors.map {|k, v| (k.to_s + ' ' + v.to_s).humanize}.join(', ')
       render :action => :new
     end
   end
