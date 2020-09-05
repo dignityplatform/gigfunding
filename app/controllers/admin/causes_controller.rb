09ae50c85cb0a095f0b8534217a2d7739bb01ef8
cpcwood <cpcwood@outlook.com>
2020-09-05 15:50:54 +0100

    add destroy method to AdminCausesController

--- app/controllers/admin/causes_controller.rb
+++ app/controllers/admin/causes_controller.rb
@@ -35,6 +35,16 @@ class Admin::CausesController < Admin::AdminBaseController
     end
   end
 
+  def destroy
+    @cause = Cause.find(params[:id])
+    if @cause.destroy()
+      redirect_to admin_causes_path, notice: 'Cause deleted'
+    else
+      flash[:error] = @cause.errors.map {|k, v| (k.to_s + ' ' + v.to_s).humanize}.join(', ')
+      render :action => :index
+    end
+  end
+
   private
 
   def set_selected_left_navi_link
