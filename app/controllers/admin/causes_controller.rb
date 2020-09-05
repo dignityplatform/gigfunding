b3deed6d65d5fda0ee62ce23f8040af16f9e12ea
cpcwood <cpcwood@outlook.com>
2020-09-05 14:57:57 +0100

    add create path to AdminCausesController

--- app/controllers/admin/causes_controller.rb
+++ app/controllers/admin/causes_controller.rb
@@ -25,6 +25,16 @@ class Admin::CausesController < Admin::AdminBaseController
     @cause = Cause.find(params[:id])
   end
 
+  def update
+    @cause = Cause.find(params[:id])
+    if @cause.update(cause_params)
+      redirect_to admin_causes_path, notice:  "Cause updated"
+    else
+      flash[:error] = @cause.errors.map {|k, v| (k.to_s + ' ' + v.to_s).humanize}.join(', ')
+      render :action => :edit
+    end
+  end
+
   private
 
   def set_selected_left_navi_link
