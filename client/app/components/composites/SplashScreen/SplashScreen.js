fa949f9a03d9665dcdeec2306eff6efa030eb7ae
cpcwood <cpcwood@outlook.com>
2020-11-16 20:18:30 +0000

    splash header created

--- client/app/components/composites/SplashScreen/SplashScreen.js
+++ client/app/components/composites/SplashScreen/SplashScreen.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-
+import SplashHeader from '../../elements/SplashHeader/SplashHeader';
 
 class SplashScreen extends Component {
   render() {
     return (
       <div className='splash-screen'>
         <img src={this.props.splashScreen.splashImageSrc} className='background-image'/>
+        <div className='wrapper'>
+          <SplashHeader/>
+        </div>
       </div>
     )
   }
---
+++ client/app/components/elements/SplashHeader/SplashHeader.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class SplashHeader extends Component {
+
+  render() {
+    return (
+      <div className='splash-header-container'>
+        <h1 className='splash-header'>
+          HIRE LOCAL SKILLS
+        </h1>
+        <div className='splash-subtitle'>
+          FUND SOCIAL
+          <br></br>
+          CHANGE
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SplashHeader
