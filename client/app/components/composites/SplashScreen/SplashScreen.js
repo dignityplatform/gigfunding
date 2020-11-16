a4546438d43d93152371160a5832269c339ec97c
cpcwood <cpcwood@outlook.com>
2020-11-16 18:05:55 +0000

    add marketplace landing page styles to rails asset pipeline, instead of rendering in client since ssr causes styles mismatch

--- client/app/components/composites/SplashScreen/SplashScreen.js
+++ client/app/components/composites/SplashScreen/SplashScreen.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
 
 
-const in_browser_env = (typeof document !== 'undefined') 
-
-if (in_browser_env) {
-  require('./SplashScreen.scss');
-}
-
 class SplashScreen extends Component {
-
   render() {
-
-    let splashImageSrc
-    if (in_browser_env) {
-      splashImageSrc = this.props.splashScreen.splashImageSrc
-    }
-
     return (
       <div className='splash-screen'>
-        <img src={splashImageSrc} className='background-image'/>
+        <img src={this.props.splashScreen.splashImageSrc} className='background-image'/>
       </div>
     )
   }
--- client/app/components/sections/MarketplaceLandingPage/MarketplaceLandingPage.js
+++ client/app/components/sections/MarketplaceLandingPage/MarketplaceLandingPage.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import SplashScreen from '../../composites/SplashScreen/SplashScreen';
 
-if (typeof document !== 'undefined') {
-  require('./MarketplaceLandingPage.scss');
-}
-
 class MarketplaceLandingPage extends Component {
 
   render() {
