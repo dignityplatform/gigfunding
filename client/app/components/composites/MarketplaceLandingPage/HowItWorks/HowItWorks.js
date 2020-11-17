b061bc9024173ac967efba373be475d8231209e1
cpcwood <cpcwood@outlook.com>
2020-11-17 14:20:29 +0000

    HowItWorks - action button added for mobile and desktop

--- client/app/components/composites/MarketplaceLandingPage/HowItWorks/HowItWorks.js
+++ client/app/components/composites/MarketplaceLandingPage/HowItWorks/HowItWorks.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import MarketplaceLandingPageSectionHeading from '../../../elements/MarketplaceLandingPage/SectionHeading';
-import MarketplaceLandingPageSubHeading from '../../../elements/MarketplaceLandingPage/SubHeading.js';
+import SectionHeading from '../../../elements/MarketplaceLandingPage/SectionHeading';
+import SubHeading from '../../../elements/MarketplaceLandingPage/SubHeading.js';
 import HowItWorksDetails from './HowItWorksDetails';
 import HowItWorksVideo from './HowItWorksVideo.js';
+import HowItWorksAction from './HowItWorksAction.js';
 
 class HowItWorks extends Component {
   render() {
     return (
       <section className='how-it-works'>
         <div className='wrapper'>
-          <MarketplaceLandingPageSectionHeading text='HOW IT WORKS'/>
+          <SectionHeading text='HOW IT WORKS'/>
           <div className='text-content-wrapper'>
-            <MarketplaceLandingPageSubHeading style='text-align: left;' text='Gigfunding is an online marketplace, where you can Hire or Volunteer skills, and all the payment is donated to causes you choose'/>
+            <SubHeading style='text-align: left;' text='Gigfunding is an online marketplace, where you can Hire or Volunteer skills, and all the payment is donated to causes you choose'/>
           </div>
           <HowItWorksVideo/>
           <HowItWorksDetails/>
+          <HowItWorksAction routes={this.props.routes}/>
         </div>
       </section>
     )
