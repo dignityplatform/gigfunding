3579a16943a2e858b495dabb33c281afd89ea5b4
cpcwood <cpcwood@outlook.com>
2020-11-17 14:55:52 +0000

    ExploreGigs - two way selector onClick functionality added

--- client/app/components/composites/MarketplaceLandingPage/ExploreGigs/ExploreGigs.js
+++ client/app/components/composites/MarketplaceLandingPage/ExploreGigs/ExploreGigs.js
@@ -3,11 +3,34 @@ import SectionHeading from '../../../elements/MarketplaceLandingPage/SectionHead
 import TwoWaySelector from './TwoWaySelector';
 
 class ExploreGigs extends Component {
+  constructor () {
+    super()
+    this.state = {
+      sectionOneSelected: true
+    }
+  }
+
+  selectSectionOne () {
+    this.setState({
+      sectionOneSelected: true
+    })
+  }
+
+  selectSectionTwo () {
+    this.setState({
+      sectionOneSelected: false
+    })
+  }
+
   render() {
     return (
       <section className='explore-gigs wrapper'>
         <SectionHeading text='EXPLORE GIGS'/>
-        <TwoWaySelector/>
+        <TwoWaySelector 
+          sectionOneSelected={this.state.sectionOneSelected} 
+          handleButtonOneClick={this.selectSectionOne.bind(this)}
+          handleButtonTwoClick={this.selectSectionTwo.bind(this)}
+        />
       </section>
     )
   }
--- client/app/components/composites/MarketplaceLandingPage/ExploreGigs/TwoWaySelector.js
+++ client/app/components/composites/MarketplaceLandingPage/ExploreGigs/TwoWaySelector.js
@@ -3,16 +3,27 @@ import Heading from '../../../elements/MarketplaceLandingPage/Heading';
 
 class TwoWaySelector extends Component {
   render() {
+
+    let buttonOneHighlight
+    let buttonTwoHighlight
+    if (!!this.props.sectionOneSelected) {
+      buttonOneHighlight = 'requesting'
+      buttonTwoHighlight = ''
+    } else {
+      buttonOneHighlight = ''
+      buttonTwoHighlight = 'offering'
+    }
+
     return (
       <div className='two-way-selector'>
         <div className='selector-container'>
-          <button className='selector-button'>
-            <Heading text='VOLUNTEER'/>
+          <button className='selector-button' onClick={this.props.handleButtonOneClick}>
+            <Heading text='VOLUNTEER' highlight={buttonOneHighlight}/>
           </button>
         </div>
         <div className='selector-container'>
-          <button className='selector-button'>
-            <Heading text='HIRE'/>
+          <button className='selector-button' onClick={this.props.handleButtonTwoClick}>
+            <Heading text='HIRE' highlight={buttonTwoHighlight} />
           </button>
         </div>
       </div>
