e7df0703863afd0c8b5433bed23051165725b27b
cpcwood <cpcwood@outlook.com>
2020-11-17 11:16:16 +0000

    HowItWorks - add subheading

--- client/app/components/composites/MarketplaceLandingPage/HowItWorks/HowItWorks.js
+++ client/app/components/composites/MarketplaceLandingPage/HowItWorks/HowItWorks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MarketplaceLandingPageSectionHeading from '../../../elements/MarketplaceLandingPage/SectionHeading';
+import MarketplaceLandingPageSubHeading from '../../../elements/MarketplaceLandingPage/SubHeading.js';
 
 class HowItWorks extends Component {
   render() {
@@ -7,6 +8,9 @@ class HowItWorks extends Component {
       <section className='how-it-works'>
         <div className='wrapper'>
           <MarketplaceLandingPageSectionHeading text='HOW IT WORKS'/>
+          <div className='text-content'>
+            <MarketplaceLandingPageSubHeading style='text-align: left;' text='Gigfunding is an online marketplace, where you can Hire or Volunteer skills, and all the payment is donated to causes you choose'/>
+          </div>
         </div>
       </section>
     )
--- client/app/components/elements/MarketplaceLandingPage/SectionHeading.js
+++ client/app/components/elements/MarketplaceLandingPage/SectionHeading.js
@@ -4,7 +4,7 @@ class MarketplaceLandingPageSectionHeading extends Component {
   render() {
     return (
       <div className='flex-container full-width'>
-        <h2 className={'marketplace-landing-page-section-heading half-highlight-element'}>
+        <h2 className='marketplace-landing-page-section-heading half-highlight-element'>
           {this.props.text}
         </h2>
       </div>
