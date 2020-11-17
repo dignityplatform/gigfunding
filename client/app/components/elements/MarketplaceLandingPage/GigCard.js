cf94700462e9fe9c8f6472d11e0c95328400161d
cpcwood <cpcwood@outlook.com>
2020-11-17 18:24:08 +0000

    ExploreGigs - gig card desktop styles

--- client/app/components/elements/MarketplaceLandingPage/GigCard.js
+++ client/app/components/elements/MarketplaceLandingPage/GigCard.js
@@ -6,24 +6,28 @@ class GigCard extends Component {
     const linkToGig = this.props.routes.listing_path(this.props.id)
     const userStatement = `${this.props.author} ${this.props.type}`
     const priceUnits = this.props.unitType === 'unit' ? '' : `/${this.props.unitType}`
-    const priceStatement = `£${this.props.price}${priceUnits}`
-    const titleStatement = this.props.listingTitle.toUpperCase()
+    const priceStatement = `£${this.props.price}${priceUnits.toUpperCase()}`
+    const titleStatement = this.props.listingTitle.length > 35 ? 
+      `${this.props.listingTitle.toUpperCase().substring(0,32)}...` : 
+      this.props.listingTitle.toUpperCase()
     const linkTitle = this.props.listingTitle
 
     return (
-      <article className='gig-card'>
-        <a href={linkToGig} title={`View ${linkTitle}`}>
-          <div className='listing-image-container'>
-            <img src={this.props.imageSrc} alt='gig listing image'/>
-          </div>
-          <div className='user-statement'>
-            {userStatement}
-          </div>
-          <div className='title-statement'>
-            {titleStatement}
-          </div>
-          <div className='price-statement'>
-            {priceStatement}
+      <article className='gig-card-container'>
+        <a href={linkToGig} title={`View ${linkTitle}`} className='gig-card-inner'>
+          <img src={this.props.imageSrc} alt='gig listing image'/>
+          <div className='details-container'>
+            <div className='user-statement'>
+              {userStatement}
+            </div>
+            <div className='price-container'>
+              <div className='title-statement'>
+                {titleStatement}
+              </div>
+              <div className='price-statement'>
+                {priceStatement}
+              </div>
+            </div>
           </div>
         </a>
       </article>
