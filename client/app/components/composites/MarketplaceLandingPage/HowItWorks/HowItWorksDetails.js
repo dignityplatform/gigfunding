7d4f77107067151c52d0efb8841d05472a848339
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2021-03-03 15:46:24 +0000

    className updated for homepage

--- client/app/components/composites/MarketplaceLandingPage/HowItWorks/HowItWorksDetails.js
+++ client/app/components/composites/MarketplaceLandingPage/HowItWorks/HowItWorksDetails.js
@@ -13,27 +13,27 @@ class HowItWorksDetails extends Component {
         <article className='how-it-works-details'>
           <Heading text={'VOLUNTEER YOUR SKILLS'} highlight={'requesting'}/>
           <TextBody>
-            We’re not quite back to normality yet. The good news is you can still virtually volunteer (pretty much) any skill you have by <span>posting a listing.</span>
+            We’re not quite back to normality yet. The good news is you can still virtually volunteer (pretty much) any skill you have by <span className="how-it-works-background">posting a listing.</span>
             <br /><br />
             So whether you’re a piano teacher, language expert or marketing consultant; you can still give in a way that you love. When someone hires you, the money they pay is donated to your favourite cause and theirs.
             <br /><br />
-            See which skills people are <span>looking to hire</span> right now.
+            See which skills people are <span className="how-it-works-background">looking to hire</span> right now.
             <br /><br />
-            <span>This is Giving by Doing.</span>
+            <span className="how-it-works-background">This is Giving by Doing.</span>
           </TextBody>
         </article>
         <article className='how-it-works-details'>
           <Heading text={'HIRE SKILLS'} highlight={'offering'}/>
           <TextBody>
-            You can hire (pretty much) any skill you want by <span><strong>posting a listing.</strong></span>
+            You can hire (pretty much) any skill you want by <span className="how-it-works-background"><strong>posting a listing.</strong></span>
             <br /><br />
             So whether you need a dog walker, accountant, or photographer;
             we'll find the person you need. The money you pay will be donated
             to your favourite cause and theirs.
             <br /><br />
-            See which skills people are <span><strong>volunteering</strong></span> right now.
+            See which skills people are <span className="how-it-works-background"><strong>volunteering</strong></span> right now.
             <br /><br />
-            <span><strong>That's fun-raising.</strong></span>
+            <span className="how-it-works-background"><strong>That's fun-raising.</strong></span>
           </TextBody>
         </article>
       </div>
