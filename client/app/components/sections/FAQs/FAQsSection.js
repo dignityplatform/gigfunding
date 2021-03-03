d55ce791ba124d63890aaacb98bd65d97de2f185
cpcwood <cpcwood@outlook.com>
2021-03-03 14:55:31 +0000

    add text section to bottom of infos_faqs page

--- client/app/components/sections/FAQs/FAQsSection.js
+++ client/app/components/sections/FAQs/FAQsSection.js
@@ -2,7 +2,9 @@ import React from 'react';
 import SectionHeader from 'components/elements/Headers/SectionHeader'
 import SubSectionMenu from 'components/composites/Menus/SubSectionMenu'
 import FAQsQuestionsList from 'components/composites/FAQs/FAQsQuestionsList'
-
+import SectionSubHeader from 'components/elements/Headers/SectionSubHeader'
+import GeneralLink from 'components/elements/Link/GeneralLink'
+import CenteredTextContent from 'components/elements/Text/CenteredTextContent'
 class FAQsSection extends React.Component {
   constructor() {
     super();
@@ -36,6 +38,12 @@ class FAQsSection extends React.Component {
             updateSelectedSection={this.updateSelectedSection.bind(this)} 
             selectedSection={this.state.selectedSection}/>
         <FAQsQuestionsList questionsSection={selectedQuestionsSection}/>
+        <SectionSubHeader classes='section-sub-header'>
+          What if my question wasn't answered here?
+        </SectionSubHeader>
+        <CenteredTextContent>
+          Where always happy to answer queries and requests. Email <GeneralLink link='mailto:team@gigfunding.org'>team@gigfunding.org</GeneralLink> and we will get back to you, and if it's a particularly brilliant question we'll add it to this very page.
+        </CenteredTextContent>
       </section>
     )
   }
